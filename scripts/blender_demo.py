5cfa880e002c2477f15ecc7f6c8089d4a25f1798
Heindl Christoph <cheind@profactor.at>
2020-08-21 10:44:37 +0200

    chnaged to flower type

--- scripts/blender_demo.py
+++ scripts/blender_demo.py
@@ -7,5 +7,5 @@ bpy.data.objects.remove(bpy.data.objects['Cube'], do_unlink=True)
 # Generate mesh
 shape=(100,100)
 obj = sshape.make_bpy_mesh(shape)
-x,y,z = sshape.supercoords(sshape.SPHERE, shape=shape)
+x,y,z = sshape.supercoords(sshape.FLOWER, shape=shape)
 sshape.update_bpy_mesh(x, y, z, obj)
